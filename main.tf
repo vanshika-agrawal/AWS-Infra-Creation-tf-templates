bd7c15f65e6bfd12e5e971bf2ab3439c0b82fbf4
vanshika-agrawal <vanshikaagrawal916@gmail.com>
2024-05-20 22:29:54 +0530

    Added VPC, NAT GATEWAY AND Route table basic config

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.50.0"
+    }
+  }
+ 
+  required_version = ">= 1.2.0"
+}
+ 
+provider "aws" {
+  region  = "us-west-2"
+}
+ 
---
+++ vpc.tf
@@ -0,0 +1,58 @@
+resource "aws_default_vpc" "main" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+#Public Subnets for AZ - 2a, 2b, 2c
+resource "aws_default_subnet" "public_subnet_1" {
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "public subnet for us-west-2a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2b"
+  tags = {
+    Name = "public subnet for us-west-2b"
+  }
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2c"
+  tags = {
+    Name = "public subnet for us-west-2c"
+  }
+}
+
+#Private Subnets for AZ - 2a, 2b, 2c
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "private subnet for us-west-2a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2b"
+  tags = {
+    Name = "private subnet for us-west-2b"
+  }
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2c"
+  tags = {
+    Name = "private subnet for us-west-2c"
+  }
+}
