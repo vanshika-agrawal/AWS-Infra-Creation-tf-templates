2589f59ff020a51ff8a4607ba570b258c01fb8c9
vanshika-agrawal <vanshikaagrawal916@gmail.com>
2024-10-09 02:01:32 +0530

    added security group and tls key changes

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ec2_ecs_cluster"{
-instance_type = 
+instance_type = "t3a.small"
 subnet_id= aws_subnet.private_subnet_1.id
-availability_zone= 
-key_name = 
+availability_zone= "us-west-2a"
+key_name = "deployer_key"
 user_data= <<-EOF
     #!/bin/bash
     echo ECS_CLUSTER=${aws_ecs_cluster.cluster.name} >> /etc/ecs/ecs.config
@@ -14,10 +14,11 @@ tags = {
     name = ECS-EC2 instance
 }
 }
-resource "aws_security_group" "ecs_sg"{
-name = "aws_ecs_sg"
-vpc_id = aws_default_vpc.main.id
-tags ={
-    Name = AWS ecs sg
+
+resource "tls_private_key" "tls_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
-}
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  }
--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.50.0"
     }
+    tls = {
+      source = "hashicorp/tls"
+      version = "4.0.6"
+    }
   }
  
   required_version = ">= 1.2.0"
@@ -12,4 +16,6 @@ terraform {
 provider "aws" {
   region  = "us-west-2"
 }
- 
+provider "tls"{
+  # No specific configuration is required for TLS
+}
